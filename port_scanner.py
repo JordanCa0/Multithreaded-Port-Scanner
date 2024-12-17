5147ce9a642031b2cde26f64774cd5117fd5d278
Jordan Cao <jordancao01@gmail.com>
2024-12-17 12:42:53 -0500

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,13 +1,19 @@
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor 
+#Allows ability to execute concurrent tasks
+#Thread pool executor does the asynchronous execution with threads 
 import socket
+#Libray for working with socets. Used to form TCP conenction. 
 import time
 
-MAX_WORKERS = 100
+MAX_WORKERS = 10000 #Max number of threads working at once 
 
 def generate_port_chunks(port_range): 
+    #Gets the min/max port numbers from port range
     port_ranges = port_range.split('-')
     port_chunks = []
+    #Divides port range into chunks 
     chunks_size = int((int(port_ranges[1])- int(port_ranges[0]))/MAX_WORKERS)
+    #Creates nested list of port chunks to be handled by each worker
 
     for i in range(MAX_WORKERS):
         start = int(port_ranges[0]) + (chunks_size * i)
@@ -17,12 +23,15 @@ def generate_port_chunks(port_range):
 
 def scan(ip_address,port_chunk):
     print(f"[-] Scanning {ip_address} from {port_chunk[0]} to {port_chunk[1]}.")
+    #Loop through min/max port chunks 
     for port in range(int(port_chunk[0]), int(port_chunk[1])):
+        #Attempt a TCP IPv4 connection to provided port and IP address
         try: 
             scan_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             scan_socket.settimeout(2)
             scan_socket.connect((ip_address,port))
             print(f"[!] Port {port} is open")
+            #If port is closed an exception will be thrown, capture it here 
         except:
             None
 
@@ -33,7 +42,7 @@ def main():
     port_chunks = generate_port_chunks(port_range)
 
     start_time = time.time()
-
+    #Submit tasks to be executed by thead pool using map
     with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor: 
         executor.map(scan, [ip_address] * len(port_chunks), port_chunks)
 
